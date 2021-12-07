dc620fd8288d5f320107603c34316940bf9f3c7d
MarkuSchick <markuschick@gmail.com>
2021-12-07 17:06:11 +0100

    feat: specify (virtual) trading token

---
+++ contracts/interfaces/IVirtualToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+// interfaces
+import {ICryptoSwap} from "../interfaces/ICryptoSwap.sol";
+import {IPerpetual} from "../interfaces/IPerpetual.sol";
+
+// dependencies
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IVirtualToken is IERC20, IERC20Metadata {
+    function mint(uint256 amount) external;
+
+    function burn(uint256 amount) external;
+}
---
+++ contracts/tokens/VirtualToken.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+// interfaces
+import {IPerpetual} from "../interfaces/IPerpetual.sol";
+import {IVirtualToken} from "../interfaces/IVirtualToken.sol";
+
+// dependencies
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// toDO: Write optimized ERC20 implementation for trades
+contract VirtualToken is IVirtualToken, ERC20 {
+    IPerpetual public immutable perpetual;
+
+    modifier onlyPerpetual() {
+        require(msg.sender == address(perpetual));
+        _;
+    }
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        IPerpetual _perpetual
+    ) ERC20(_name, _symbol) {
+        perpetual = _perpetual;
+    }
+
+    function mint(uint256 amount) external override onlyPerpetual {
+        _mint(address(perpetual), amount);
+    }
+
+    function burn(uint256 amount) external override onlyPerpetual {
+        _burn(address(perpetual), amount);
+    }
+}
